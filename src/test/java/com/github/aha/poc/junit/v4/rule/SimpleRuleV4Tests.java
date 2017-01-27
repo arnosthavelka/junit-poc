1bb198f8257ced1f028734116b20cc4896379de2
Arnošt Havelka <arnost.havelka@gmail.com>
2017-01-27 17:06:41 +0100

    + example of suite usage for V4

--- src/test/java/com/github/aha/poc/junit/v4/rule/SimpleRuleV4Tests.java
+++ src/test/java/com/github/aha/poc/junit/v4/rule/SimpleRuleV4Tests.java
@@ -4,12 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
 
-import org.junit.AssumptionViolatedException;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestRule;
-import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,26 +12,6 @@ public class SimpleRuleV4Tests {
 
 	/** Logger. */
 	private static final Logger LOG = LoggerFactory.getLogger(SimpleRuleV4Tests.class);
-
-	/**
-	 * Log method execution (start & stop).
-	 */
-	@Rule
-	public TestRule watcher = new TestWatcher() {
-		
-		protected void starting(Description description) {
-			LOG.debug("Starting test {}#{}", description.getTestClass().getSimpleName(), description.getMethodName());
-		}
-
-		protected void finished(Description description) {
-			LOG.debug("Finished test {}#{}", description.getTestClass().getSimpleName(), description.getMethodName());
-		}
-
-		protected void skipped(AssumptionViolatedException e, Description description) {
-			LOG.debug("Skipped test {}#{}", description.getTestClass(), description.getMethodName());
-		}
-
-	};
 
 	@Test
 	public void testOk() {
