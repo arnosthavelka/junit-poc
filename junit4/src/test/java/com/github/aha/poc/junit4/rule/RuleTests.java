706a7e6bd79967e6f3180949182a5bbc2d433000
Arnošt Havelka <arnost.havelka@gmail.com>
2017-01-30 14:47:53 +0100

    - packages renamed to follow multi-project structure

---
+++ junit4/src/test/java/com/github/aha/poc/junit4/rule/RuleTests.java
@@ -0,0 +1,50 @@
+package com.github.aha.poc.junit4.rule;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+
+import org.junit.AssumptionViolatedException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RuleTests {
+
+	/** Logger. */
+	private static final Logger LOG = LoggerFactory.getLogger(RuleTests.class);	
+	/**
+	 * Log method execution (start & stop).
+	 */
+	@Rule
+	public TestRule watcher = new TestWatcher() {
+		
+		protected void starting(Description description) {
+			LOG.debug("Starting test {}#{}", description.getTestClass().getSimpleName(), description.getMethodName());
+		}
+
+		protected void finished(Description description) {
+			LOG.debug("Finished test {}#{}", description.getTestClass().getSimpleName(), description.getMethodName());
+		}
+
+		protected void skipped(AssumptionViolatedException e, Description description) {
+			LOG.debug("Skipped test {}#{}", description.getTestClass(), description.getMethodName());
+		}
+
+	};
+
+	@Test
+	public void testOk() {
+		assertThat(5 + 5, equalTo(10));
+	}
+
+	@Test
+	public void testFailure() {
+		assertThat(5 + 6, not(equalTo(10)));
+	}	
+
+}
---
+++ junit4/src/test/java/com/github/aha/poc/junit4/suite/SuiteTests.java
@@ -0,0 +1,29 @@
+package com.github.aha.poc.junit4.suite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.aha.poc.junit4.suite.tests.AddTests;
+import com.github.aha.poc.junit4.suite.tests.MultiplyTests;
+
+// https://examples.javacodegeeks.com/core-java/junit/junit-suite-test-example/
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	AddTests.class,
+	MultiplyTests.class
+})
+public class SuiteTests {
+
+	/** Logger. */
+	private static final Logger LOG = LoggerFactory.getLogger(SuiteTests.class);
+
+	@Test
+	public void notExecuted() {
+		LOG.debug("Do you see me?");
+	}
+
+}
