356c5471953598389298f5f09327035e098845b8
Arnošt Havelka <arnost.havelka@gmail.com>
2017-02-03 12:21:33 +0100

    + [ju-unit4] example of validation od test annotations

---
+++ junit4/src/test/java/com/github/aha/poc/junit4/validation/AbstractValidationTest.java
@@ -0,0 +1,10 @@
+package com.github.aha.poc.junit4.validation;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+
+@RunWith(Categories.class)
+@NoExclusion
+public abstract class AbstractValidationTest {
+
+}
---
+++ junit4/src/test/java/com/github/aha/poc/junit4/validation/NoExclusion.java
@@ -0,0 +1,14 @@
+package com.github.aha.poc.junit4.validation;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.junit.validator.ValidateWith;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ValidateWith(NoExclusionValidation.class)
+public @interface NoExclusion {
+
+}
