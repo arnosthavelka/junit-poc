f56037fd2d3a4978e9950fb5e49b775e780b7581
Arnošt Havelka <arnost.havelka@gmail.com>
2017-02-23 10:52:44 +0100

    - [ju-junit5] cleaning

--- junit5/src/test/java/com/github/aha/poc/junit5/intro/DisabledTest.java
+++ junit5/src/test/java/com/github/aha/poc/junit5/intro/DisabledTest.java
@@ -6,10 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 
-@RunWith(JUnitPlatform.class)
 public class DisabledTest {
 
 	@Test
