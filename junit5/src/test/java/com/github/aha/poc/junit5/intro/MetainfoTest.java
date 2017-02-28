41162085b75e45ae018b041f797a0438be40e8b0
Arnošt Havelka <arnost.havelka@gmail.com>
2017-02-28 16:03:30 +0100

    + [ju-junit5] example of TestInfo and TestReport usage

---
+++ junit5/src/test/java/com/github/aha/poc/junit5/intro/MetainfoTest.java
@@ -0,0 +1,51 @@
+package com.github.aha.poc.junit5.intro;
+
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.aha.poc.junit5.tag.TagConsts;
+
+@RunWith(JUnitPlatform.class)
+@DisplayName("A meta-info usage")
+public class MetainfoTest {
+
+	/** Logger. */
+	private static final Logger LOG = LoggerFactory.getLogger(MetainfoTest.class);
+	
+	@Test
+	@DisplayName("show UT meta-info data")
+	@Tag(TagConsts.A)
+	@Tag(TagConsts.FAST)
+	void useTestInfo(TestInfo ti) {
+		LOG.info("Test name={}", ti.getTestMethod().get().getName());
+		LOG.info("Display name={}", ti.getDisplayName());
+		ti.getTags().stream().forEach(t -> LOG.info("\ttag={}", t));
+	}
+
+	@Test
+	@DisplayName("add single value to the test report")
+	void useTestReporterWithSingleParam(TestReporter tr) {
+        tr.publishEntry("developer", "Arny");
+	}
+
+	@Test
+	@DisplayName("add more values to the test report")
+	void useTestReporterWithMultiParams(TestInfo ti, TestReporter tr) {
+        HashMap<String, String> values = new HashMap<>();
+        values.put("name", ti.getTestMethod().get().getName());
+        values.put("developer", "Arny");
+        values.put("poc", "JUnit5");
+        tr.publishEntry(values);
+	}
+
+}
