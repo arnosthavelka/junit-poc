d73903b2b47289cf0652b60cd7836191a753626c
Arnošt Havelka <arnost.havelka@gmail.com>
2017-03-02 16:22:38 +0100

    + [ju-junit5] move of extention example to package com.github.aha.poc.junit5.extendwith.simple

---
+++ junit5/src/test/java/com/github/aha/poc/junit5/extendwith/simple/CustomAfterEach.java
@@ -0,0 +1,18 @@
+package com.github.aha.poc.junit5.extendwith.simple;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.TestExtensionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomAfterEach implements AfterEachCallback {
+
+	/** Logger. */
+	private static final Logger LOG = LoggerFactory.getLogger(CustomAfterEach.class);
+	
+	@Override
+	public void afterEach(TestExtensionContext context) throws Exception {
+		LOG.info("After method '{}'", context.getTestMethod().get().getName());
+	}
+
+}
