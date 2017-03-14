a95ac1218f115805970b692ceaf1b4b87c957642
Arnošt Havelka <arnost.havelka@gmail.com>
2017-03-14 08:57:22 +0100

    - [ju-junit4] fix of custom annotation (to be usable only on methods)

--- junit4/src/test/java/com/github/aha/poc/junit4/runner/theory/TestedOnTests.java
+++ junit4/src/test/java/com/github/aha/poc/junit4/runner/theory/TestedOnTests.java
@@ -8,20 +8,26 @@ import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.experimental.theories.suppliers.TestedOn;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // https://github.com/junit-team/junit4/wiki/Theories
 
 @RunWith(Theories.class)
 public class TestedOnTests {
 
+	/** Logger. */
+	private static final Logger LOG = LoggerFactory.getLogger(TestedOnTests.class);
+
 	/**
-	 * Prove theory: "every B can be divide by every A without any remaining" 
+	 * Prove theory: "every A is the aliquot number for any B number (i.e. theorem of B / A has no remaining)" 
 	 */
     @Theory
-	public void multiplication(
+	public void aliquotTest(
 			@TestedOn(ints = {2, 4}) int a,
 	        @TestedOn(ints = {8, 16, 24, }) int b
 	    ) {
+    	LOG.debug("values: a={}, b={}", a, b);
       	assertThat((b / a) * a , is(equalTo(b)));
 	}
 
