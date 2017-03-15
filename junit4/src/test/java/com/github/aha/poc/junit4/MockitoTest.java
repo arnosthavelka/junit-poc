41177be8407f001d7c411a744556e5e5eefc2d8b
Arnošt Havelka <arnost.havelka@gmail.com>
2017-03-15 11:10:15 +0100

    - [ju-junit4] fix of mockito usage (wrong finish method)

---
+++ junit4/src/test/java/com/github/aha/poc/junit4/MockitoTest.java
@@ -0,0 +1,34 @@
+package com.github.aha.poc.junit4;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MockitoTest {
+
+	private static Calculator calc;
+
+	@Before
+	public void init() {
+		calc = mock(Calculator.class);
+		when(calc.add(2, 3)).thenReturn(5);
+	}
+	
+	@After
+	public void finish() {
+		verify(calc).add(2, 3);
+	}
+	
+	@Test
+	public void simpleTest() {
+		assertThat(calc.add(2, 3), is(equalTo(5)));
+	}
+
+}
