fc8627ba45902ca07c063d698b668099679c4569
Arnošt Havelka <arnost.havelka@gmail.com>
2018-03-20 08:05:23 +0100

    + [JU5] basic tests switched to AssertJ usage

--- junit5/src/test/java/com/github/aha/poc/junit5/BasicTests.java
+++ junit5/src/test/java/com/github/aha/poc/junit5/BasicTests.java
@@ -1,8 +1,6 @@
 package com.github.aha.poc.junit5;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -30,24 +28,21 @@ public class BasicTests {
 	@DisplayName("validate tags existence via JUnit")
 	void allJUnitAssert(TestInfo testInfo) {
 		Set<String> tags = testInfo.getTags();
-        assertAll(
-                () -> assertNotNull(tags),
-                () -> assertEquals(0, tags.size())
-        );		
-	}	
-	
+		assertAll(() -> assertNotNull(tags), () -> assertEquals(0, tags.size()));
+	}
+
 	@Test
-	@DisplayName("show simple assert via Hamcrest")
+	@DisplayName("show simple assert via AssertJ")
 	public void simpleHamcrestAssert() {
-		assertThat(true, equalTo(true));
+		assertThat(true).isTrue();
 	}
 
 	@Test
 	@DisplayName("check TestInfo feature")
 	void checkMethodName(TestInfo testInfo) {
 		Optional<Method> testMethod = testInfo.getTestMethod();
-		assertThat(testMethod.isPresent(), equalTo(true));
-		assertThat("TestInfo is not injected correctly", "checkMethodName", equalTo(testMethod.get().getName()));
+		assertThat(testMethod.isPresent()).isTrue();
+		assertThat(testMethod.get().getName()).isEqualTo("checkMethodName");
 	}
 
 	@Test
@@ -55,11 +50,11 @@ public class BasicTests {
 	@Tag("JUnit5")
 	void checkTag(TestInfo testInfo) {
 		Set<String> tags = testInfo.getTags();
-		assertThat(tags, notNullValue());
-		assertThat(tags.size(), equalTo(1));
+		assertThat(tags).isNotNull();
+		assertThat(tags.size()).isEqualTo(1);
 		Optional<String> firstTag = tags.stream().findFirst();
-		assertThat(firstTag.isPresent(), equalTo(true));
-		assertThat(firstTag.get(), equalTo("JUnit5"));
+		assertThat(firstTag.isPresent()).isTrue();
+		assertThat(firstTag.get()).isEqualTo("JUnit5");
 	}
 
 }
