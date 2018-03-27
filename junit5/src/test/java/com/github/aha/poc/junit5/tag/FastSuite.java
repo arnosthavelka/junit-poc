966dfeaaf9b51bda71b7d00c847c473b357a6af8
Arnošt Havelka <arnost.havelka@gmail.com>
2018-03-27 21:33:13 +0200

    + [ju5] discovery & filtering example (Launcher)

--- junit5/src/test/java/com/github/aha/poc/junit5/tag/FastSuite.java
+++ junit5/src/test/java/com/github/aha/poc/junit5/tag/FastSuite.java
@@ -1,15 +1,47 @@
 package com.github.aha.poc.junit5.tag;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.IncludeTags;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.runner.RunWith;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
+import static org.junit.platform.launcher.TagFilter.includeTags;
 
-@RunWith(JUnitPlatform.class)
-@DisplayName("Fast Suite - run all fast tests")
-@SelectClasses({ATest.class, BTest.class})
-@IncludeTags(TagConsts.FAST)
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TestIdentifier;
+import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FastSuite {
 
+	/** Logger. */
+	private static final Logger LOG = LoggerFactory.getLogger(FastSuite.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+				.selectors(selectPackage("com.github.aha.poc.junit5.tag"))
+//				.selectors(selectClass(ATest.class), selectClass(BTest.class))
+				.filters(includeTags(TagConsts.A, TagConsts.FAST))
+				.build();
+
+		Launcher launcher = LauncherFactory.create();
+
+		// analyze test plan
+		TestPlan testPlan = launcher.discover(request);
+		for (TestIdentifier testIdentifier : testPlan.getRoots()) {
+			LOG.info("Test plan contains {}:", testIdentifier.getUniqueId());
+			for (TestIdentifier ti : testPlan.getChildren(testIdentifier)) {
+				LOG.info("{}", ti.getDisplayName());
+			}
+		}
+
+		// Executing tests
+		SummaryGeneratingListener listener = new SummaryGeneratingListener();
+		launcher.registerTestExecutionListeners(listener);
+		launcher.execute(request, listener);
+	}
 }
