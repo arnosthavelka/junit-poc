bf351485950968c65b0eae93a83cfcd69710d515
Arnošt Havelka <arnost.havelka@gmail.com>
2018-04-05 21:38:51 +0200

    - [ju5] examples on suite API improved

--- junit5/src/test/java/com/github/aha/poc/junit5/intro/HelloWorldTest.java
+++ junit5/src/test/java/com/github/aha/poc/junit5/intro/HelloWorldTest.java
@@ -1,17 +1,16 @@
 package com.github.aha.poc.junit5.intro;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 
-@RunWith(JUnitPlatform.class)
 public class HelloWorldTest {
 
 	@Test
 	void helloWorld(TestInfo testInfo) {
-		System.out.println("Hello world!");
+		assertEquals(2, 1 + 1);
 	}
 
 }
