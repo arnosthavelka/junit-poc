8d1cd2fde3de346946ac39e5525ee62e22db6df6
Arnošt Havelka <arnost.havelka@gmail.com>
2018-04-19 08:08:17 +0200

    + [ju5] improvement in parameterized tests

--- junit5/src/test/java/com/github/aha/poc/junit5/params/SimpleParamTests.java
+++ junit5/src/test/java/com/github/aha/poc/junit5/params/SimpleParamTests.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -12,7 +12,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Parameterized tests in JUnit5")
-public class SimpleTests {
+public class SimpleParamTests {
 
 	@ParameterizedTest
 	@ValueSource(strings = { "Hello", "World" })
@@ -50,24 +50,24 @@ public class SimpleTests {
 
 	@ParameterizedTest
 	@CsvSource({ "Rum, 70", "Whiskey, 50", "'Gin Fizz', 150" })
-	@DisplayName("check CSV values")
-	void csvTest(String key, int value) {
+	@DisplayName("check CSV values as independet arguments")
+	void csvSimpleTest(String key, int value) {
 		assertThat(key).isNotNull();
 		assertThat(value).isGreaterThan(0);
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "Rum, 70", "Whiskey, 50", "'Gin Fizz', 150" })
+	@DisplayName("check CSV values with ArgumentsAccessor")
+	void csvSimpleTest(ArgumentsAccessor aa) {
+		assertThat(aa.getString(0)).isNotNull();
+		assertThat(aa.getInteger(1)).isGreaterThan(0);
 	}
 
 	@ParameterizedTest
 	@ArgumentsSource(CustomProvider.class)
 	@DisplayName("check provider values")
 	void providerTest(ParamTypes value) {
-		assertThat(value).isNotNull();
-		assertThat(value).isInstanceOf(ParamTypes.class);
-	}
-
-	@ParameterizedTest(name = "index={index}, source={0}")
-	@ValueSource(strings = { "i", "L", "X" })
-	@DisplayName("check converter feature")
-	void converterTest(@ConvertWith(CustomConverter.class) ParamTypes value) {
 		assertThat(value).isNotNull();
 		assertThat(value).isInstanceOf(ParamTypes.class);
 	}
