dbaf6ec1d3a8a017d0ca1b335564ef32b43e73e5
Arnošt Havelka <arnost.havelka@gmail.com>
2018-05-17 21:51:21 +0200

    + [ju5] FlagConverterTest updated

--- junit5/src/test/java/com/github/aha/poc/junit5/nested/FlagConverterTest.java
+++ junit5/src/test/java/com/github/aha/poc/junit5/nested/FlagConverterTest.java
@@ -5,36 +5,34 @@ import static com.github.aha.poc.junit5.nested.FlagConverter.FLAG_ON;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.sql.SQLException;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Verify conversions by FlagConverter")
+@DisplayName("Verify that FlagConverter")
 public class FlagConverterTest {
 
 	private FlagConverter converter = new FlagConverter();
 
 	@Nested
 	@DisplayName("should convert from a flag to the value")
-	class ConversionToFlag {
+	class ConversionFromFlag {
 
 		@Test
 		@DisplayName("convert true flag to 'Y'")
-		void convertTrueToY() throws SQLException {
+		void convertTrueToY() {
 			assertThat(converter.convertToFlag(true)).isEqualTo(FLAG_ON);
 		}
 
 		@Test
 		@DisplayName("convert false flag to 'N'")
-		void convertFalseToN() throws SQLException {
+		void convertFalseToN() {
 			assertThat(converter.convertToFlag(false)).isEqualTo(FLAG_OFF);
 		}
 
 		@Test
-		@DisplayName("don't accept NULL value")
-		void raiseExceptionForNullValue() throws SQLException {
+		@DisplayName("fail for NULL value")
+		void failForNullValue() {
 			assertThrows(RuntimeException.class, () -> converter.convertToFlag(null));
 		}
 
@@ -46,19 +44,19 @@ public class FlagConverterTest {
 
 		@Test
 		@DisplayName("convert 'Y' to flag true")
-		void convertYToTrue() throws SQLException {
+		void convertYToTrue() {
 			assertThat(converter.convertFromValue(FLAG_ON)).isTrue();
 		}
 
 		@Test
 		@DisplayName("convert 'N' to flag false")
-		void convertNToFalse() throws SQLException {
+		void convertNToFalse() {
 			assertThat(converter.convertFromValue(FLAG_OFF)).isFalse();
 		}
 
 		@Test
-		@DisplayName("don't accept NULL value")
-		void raiseExceptionForNullValue() throws SQLException {
+		@DisplayName("fail with NULL value")
+		void failForNullValue() {
 			assertThrows(RuntimeException.class, () -> converter.convertFromValue(null));
 		}
 
