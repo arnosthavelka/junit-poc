c4ba7cf83820e8c717b3ef3cb1ced4993320d7ec
Arnošt Havelka <arnost.havelka@gmail.com>
2018-05-22 22:26:56 +0200

    + [ju5] example of programmatically registered extension V2

---
+++ junit5/src/test/java/com/github/aha/poc/junit5/extension/registerextension/RegisterExtensionTest.java
@@ -0,0 +1,31 @@
+package com.github.aha.poc.junit5.extension.registerextension;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RegisterExtensionTest {
+
+	/** Logger. */
+	private static final Logger LOG = LoggerFactory.getLogger(RegisterExtensionTest.class);
+
+	@RegisterExtension
+	static TempFileSystem tempFS = new TempFileSystem("ju5-fs-");
+
+	@RegisterExtension
+	TempFileGenerator tempGenerator = new TempFileGenerator(tempFS);
+
+	@Test
+	public void test1() {
+		LOG.info("First temporary file={}", tempGenerator.getTempFile());
+
+	}
+
+	@Test
+	public void test2() {
+		LOG.info("Second temporary file={}", tempGenerator.getTempFile());
+
+	}
+
+}
