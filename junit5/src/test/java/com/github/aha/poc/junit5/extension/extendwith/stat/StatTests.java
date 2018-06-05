922d11dd95f39b15048305e0597878fb21522471
Arnošt Havelka <arnost.havelka@gmail.com>
2018-06-05 08:56:50 +0200

    + [ju5] refactoring of complex example for extension

--- junit5/src/test/java/com/github/aha/poc/junit5/extension/extendwith/stat/StatTests.java
+++ junit5/src/test/java/com/github/aha/poc/junit5/extension/extendwith/stat/StatTests.java
@@ -2,44 +2,19 @@ package com.github.aha.poc.junit5.extension.extendwith.stat;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Random;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Stat
 public class StatTests {
 
-	private static final Logger LOG = LoggerFactory.getLogger(StatTests.class);
-
-	private static Random rg;
-
-	@BeforeAll
-	static void init() {
-		rg = new Random();
-	}
-
 	@Test
 	void trueTest() {
-		randomWait();
 		assertThat(true).isTrue();
 	}
 
 	@Test
 	void falseTest() {
-		randomWait();
 		assertThat(1 == 2).isFalse();
-	}
-
-	private void randomWait() {
-		int waitTime = rg.nextInt(100);
-		try {
-			Thread.sleep(waitTime);
-		} catch (InterruptedException e) {
-			LOG.error("Sleep interuppted ...", e);
-		}
 	}
 
 }
