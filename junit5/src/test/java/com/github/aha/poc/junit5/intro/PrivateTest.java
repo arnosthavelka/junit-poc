391e76260fd4cbcf0aef1e525574a20986c0a62d
Arnošt Havelka <arnost.havelka@gmail.com>
2018-06-16 07:17:02 +0200

    + [ju5] new test for private constructor

---
+++ junit5/src/test/java/com/github/aha/poc/junit5/intro/PrivateTest.java
@@ -0,0 +1,18 @@
+package com.github.aha.poc.junit5.intro;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.github.aha.poc.junit5.Calculator;
+
+public class PrivateTest {
+
+	@Test
+	@DisplayName("check hidden private constructor")
+	public void testPrivateConstructor() {
+		assertThrows(IllegalAccessException.class, () -> Calculator.class.newInstance());
+	}
+
+}
