75840d6e5f986708152a17dabcb493c44be5535d
Arnošt Havelka <arnost.havelka@gmail.com>
2018-06-21 07:48:54 +0200

    + [spring-boot] code cleaning

--- spring-boot/src/test/java/com/github/aha/poc/junit/springboot/ServiceTests.java
+++ spring-boot/src/test/java/com/github/aha/poc/junit/springboot/ServiceTests.java
@@ -4,13 +4,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class ServiceTests {
 
