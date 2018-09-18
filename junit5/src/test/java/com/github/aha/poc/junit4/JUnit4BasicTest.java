03adab1969c6b2cf3091140bc227fc33eb9ef872
hh310 <arnost.havelka@deutsche-boerse.com>
2018-09-18 08:51:10 +0200

    + [ju5] preparation to move some test from JU4

---
+++ junit5/src/test/java/com/github/aha/poc/junit4/JUnit4BasicTest.java
@@ -0,0 +1,39 @@
+package com.github.aha.poc.junit4;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//@RunWith(JUnit4.class)
+public class JUnit4BasicTest {
+
+	private static final Logger LOG = LoggerFactory.getLogger(JUnit4BasicTest.class);
+
+	@Test
+	public void simpleTest() {
+		LOG.debug("simpleTest");
+		assertTrue(true);
+	}
+
+	@Test
+	@Ignore
+	public void disabledTest() {
+		LOG.debug("simpleTest");
+		assertThat(true, is(equalTo(true)));
+	}
+
+	@Test
+	public void simpleHamcrestAssert() {
+		assertThat(true, is(true));
+		assertThat(1 + 1, equalTo(2));
+		assertThat("hello", not(equalTo("hi")));
+	}
+
+}
