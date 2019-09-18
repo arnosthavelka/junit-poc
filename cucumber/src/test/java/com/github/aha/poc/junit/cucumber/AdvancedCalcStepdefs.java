9a699c422b3d9ca9a37f0a85960a61b5b9dd66de
hh310 <arnost.havelka@deutsche-boerse.com>
2019-09-18 08:27:58 +0200

    - update of Cucumber imports (after upgrade)

--- cucumber/src/test/java/com/github/aha/poc/junit/cucumber/AdvancedCalcStepdefs.java
+++ cucumber/src/test/java/com/github/aha/poc/junit/cucumber/AdvancedCalcStepdefs.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 import com.github.aha.poc.junit.cucumber.AdvancedCalculator.Operation;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class AdvancedCalcStepdefs {
 
