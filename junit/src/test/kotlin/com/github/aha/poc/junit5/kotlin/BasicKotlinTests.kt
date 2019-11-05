eb18c024d2d6ab9f30d518c4815c761b2a2d619c
hh310 <arnost.havelka@deutsche-boerse.com>
2019-11-05 08:21:16 +0100

    - code cleaning

--- junit/src/test/kotlin/com/github/aha/poc/junit5/kotlin/BasicKotlinTests.kt
+++ junit/src/test/kotlin/com/github/aha/poc/junit5/kotlin/BasicKotlinTests.kt
@@ -1,6 +1,6 @@
 package com.github.aha.poc.junit5.kotlin
 
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -14,7 +14,7 @@ class BasicKotlinTests {
 	@Test
 	fun `isEmpty should return true for empty lists`() {
 		val list = listOf<String>()
-		Assertions.assertTrue(list::isEmpty)
+		assertTrue(list::isEmpty)
 	}
 
 }
