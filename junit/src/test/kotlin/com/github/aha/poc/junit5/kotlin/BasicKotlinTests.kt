4e1ae10e428bb0fc4ce32ad12d7f55ada22ce7b1
hh310 <arnost.havelka@deutsche-boerse.com>
2019-11-28 09:35:00 +0100

    #4 some more kotlin test examples

--- junit/src/test/kotlin/com/github/aha/poc/junit5/kotlin/BasicKotlinTests.kt
+++ junit/src/test/kotlin/com/github/aha/poc/junit5/kotlin/BasicKotlinTests.kt
@@ -1,20 +1,32 @@
 package com.github.aha.poc.junit5.kotlin
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
+
+// https://www.baeldung.com/junit-5-kotlin
 class BasicKotlinTests {
 
 	@Test
 	fun `simple assert with Kotlin`() {
-		assertThat(0).isEqualTo(0);
+		assertThat(0).isEqualTo(0)
 	}
 
 	@Test
 	fun `isEmpty should return true for empty lists`() {
 		val list = listOf<String>()
-		assertTrue(list::isEmpty)
+		assertTrue(list::isEmpty) {
+			"list is not empty"
+		}
+	}
+
+	@Test
+	fun `3 is equal to 4`() {
+		assertThrows(ArithmeticException::class.java) {
+			5 / 0
+		} 
 	}
 
 }
