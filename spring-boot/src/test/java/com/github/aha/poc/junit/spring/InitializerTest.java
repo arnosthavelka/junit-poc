e26a3fcf1e5d2a2caca07bd77279d0a6325890cb
hh310 <arnost.havelka@deutsche-boerse.com>
2020-04-02 08:12:32 +0200

    #30 InitializerTest added

---
+++ spring-boot/src/test/java/com/github/aha/poc/junit/spring/InitializerTest.java
@@ -0,0 +1,37 @@
+package com.github.aha.poc.junit.spring;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(initializers = InitializerTest.Initializer.class)
+@DisplayName("Initialize context manually")
+public class InitializerTest {
+
+	@Autowired
+	HelloService service;
+
+	@Test
+	public void test() {
+		assertThat(service).isNotNull();
+		assertThat(service.getMessage()).isEqualTo("Hello world!");
+	}
+
+	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+		@Override
+		public void initialize(ConfigurableApplicationContext context) {
+			context.getBeanFactory().registerSingleton("helloService", new HelloService());
+		}
+
+	}
+
+}
