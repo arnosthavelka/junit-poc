28522724e6dc6f7a66bd1e35a39e86a01d8dc408
Arnost Havelka <arnost.havelka@deutsche-boerse.com>
2023-02-21 09:01:04 +0100

    bump up to Spring Boot 3 and other dependencies

--- spring-boot/src/main/java/com/github/aha/poc/junit/springboot/City.java
+++ spring-boot/src/main/java/com/github/aha/poc/junit/springboot/City.java
@@ -1,10 +1,9 @@
 package com.github.aha.poc.junit.springboot;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
--- spring-boot/src/test/java/com/github/aha/poc/junit/springboot/rest/assured/CityControllerRestAssuredIT.java
+++ spring-boot/src/test/java/com/github/aha/poc/junit/springboot/rest/assured/CityControllerRestAssuredIT.java
@@ -10,33 +10,29 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import com.github.aha.poc.junit.springboot.CityResource;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Disabled // FIXME not working in mvn install
 public class CityControllerRestAssuredIT {
-	
+
 	@LocalServerPort
 	private int port;
 
 	@Test
 	public void listCities() {
-		when()
-			.get("http://localhost:" + port + "/cities/")
-		.then()
-			.statusCode(200)
-			.assertThat().body(
-					"_embedded.cityResources.size()", is(4),
-					"_embedded.cityResources[2].id", equalTo(3),
-					"_embedded.cityResources[2].name", equalTo("Paris"));
+		when().get("http://localhost:" + port + "/cities/").then().statusCode(200).assertThat().body(
+				"_embedded.cityResources.size()", is(4), "_embedded.cityResources[2].id", equalTo(3),
+				"_embedded.cityResources[2].name", equalTo("Paris"));
 	}
 
 	@Test
 	public void getCity() {
 		long cityId = 2L;
-		CityResource city = get("http://localhost:" + port + "/cities/" + cityId).then().extract().as(CityResource.class);
+		CityResource city = get("http://localhost:" + port + "/cities/" + cityId).then().extract()
+				.as(CityResource.class);
 		assertThat(city.getId()).isEqualTo(cityId);
 		assertThat(city.getName()).isEqualTo("London");
 		assertThat(city.getCountry()).isEqualTo("Great Britain");
