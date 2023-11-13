0599635395c90b6bbb9dd5d8568ee0c6b2837712
Arnost Havelka <arnost.havelka@clearstream.com>
2023-11-13 09:00:28 +0100

    nextMultipleIds feature added

--- junit/src/main/java/com/github/aha/poc/junit/person/SequenceGenerator.java
+++ junit/src/main/java/com/github/aha/poc/junit/person/SequenceGenerator.java
@@ -1,5 +1,7 @@
 package com.github.aha.poc.junit.person;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import lombok.experimental.UtilityClass;
@@ -16,4 +18,13 @@ public class SequenceGenerator {
 	public static int nextId() {
 		return counter.getAndIncrement();
 	}
+
+	public static List<Integer> nextMultipleIds(int count) {
+		var newValues = new ArrayList<Integer>(count);
+		for (int i = 0; i < count; i++) {
+			newValues.add(counter.getAndIncrement());
+		}
+		return newValues;
+	}
+
 }
--- junit/src/test/java/com/github/aha/poc/junit5/integration/mockito/StaticUsageTests.java
+++ junit/src/test/java/com/github/aha/poc/junit5/integration/mockito/StaticUsageTests.java
@@ -3,6 +3,8 @@ package com.github.aha.poc.junit5.integration.mockito;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mockStatic;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
@@ -12,11 +14,22 @@ class StaticUsageTests {
 
 	@Test
 	void mockStaticNoArgValue() {
-	    try (MockedStatic<SequenceGenerator> utilities = mockStatic(SequenceGenerator.class)) {
-	        utilities.when(SequenceGenerator::nextId).thenReturn(5);
-	        
-	        assertThat(SequenceGenerator.nextId()).isEqualTo(5);
-	    }
+		try (MockedStatic<SequenceGenerator> seqGeneratorMock = mockStatic(SequenceGenerator.class)) {
+			seqGeneratorMock.when(SequenceGenerator::nextId).thenReturn(5);
+
+			assertThat(SequenceGenerator.nextId()).isEqualTo(5);
+		}
 	}
 
+	@Test
+	void mockStaticWithArgValue() {
+		try (MockedStatic<SequenceGenerator> seqGeneratorMock = mockStatic(SequenceGenerator.class)) {
+			int newValuesSize = 5;
+			seqGeneratorMock.when(() -> SequenceGenerator.nextMultipleIds(newValuesSize))
+					.thenReturn(List.of(1, 2, 3, 4, 5));
+
+			assertThat(SequenceGenerator.nextMultipleIds(newValuesSize)).hasSize(newValuesSize);
+		}
+	}
+	
 }
